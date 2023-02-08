47aa1cb45b4e062f1c64638c4d34da9522553b04
Saptarshi Mondal <93252111+Me7rishi@users.noreply.github.com>
2023-02-08 23:28:54 +0530

    Create Jump_Game_II.cpp

---
+++ Day36/Jump_Game_II.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+
+        int n = nums.size();
+        if (n < 2) return 0;
+        int curMax = 0, nextMax = 0, jumps = 0;
+        for (int i = 0; i < n - 1; i++) {
+            nextMax = max(nextMax, i + nums[i]);
+            if (i == curMax){
+                jumps++;
+                curMax = nextMax;
+                if (curMax >= n - 1) 
+                    break;
+            }
+        }
+        return jumps;
+    }
+};
