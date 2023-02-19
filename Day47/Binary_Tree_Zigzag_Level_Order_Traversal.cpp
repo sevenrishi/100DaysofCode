8bd99c6561a5797221f9a199c495bbf35b522ae2
Saptarshi Mondal <93252111+Me7rishi@users.noreply.github.com>
2023-02-19 21:48:53 +0530

    Create Binary_Tree_Zigzag_Level_Order_Traversal.cpp

---
+++ Day47/Binary_Tree_Zigzag_Level_Order_Traversal.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+
+        vector<vector<int>> ans;
+        queue<TreeNode*> q;
+        if(root==NULL){
+            return ans;
+        }
+        q.push(root);
+        int f=0;
+        while(q.size()){
+            vector<int> temp;
+            int si=q.size();
+            while(si--){
+                TreeNode* te=q.front();
+                q.pop();
+                if(te->left!=NULL){
+                    q.push(te->left);
+                }
+                if(te->right!=NULL){
+                    q.push(te->right);
+                }
+                temp.push_back(te->val);
+
+            }
+            if(f==0){
+                f=1;    
+                ans.push_back(temp);
+            }
+            else{
+                reverse(temp.begin(),temp.end());
+                ans.push_back(temp);
+                f=0;
+            }
+        }
+        return ans;
+    }
+};
