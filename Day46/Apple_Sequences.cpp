57d4197712e589de1e5be91dfc57b38659f77c97
Saptarshi Mondal <93252111+Me7rishi@users.noreply.github.com>
2023-02-19 21:42:38 +0530

    Update Apple_Sequences.cpp

--- Day46/Apple_Sequences.cpp
+++ Day46/Apple_Sequences.cpp
@@ -1,5 +1,34 @@
-Solutions{
-  Public:
-     int AppleSequences(){
-     }
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{   
+public:
+    int appleSequences(int n, int m, string arr){
+        // code here 
+    }
 };
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N, M;
+        cin >> N >> M;
+        string arr;
+        cin >> arr;
+        
+        Solution ob;
+        cout << ob.appleSequences(N, M, arr) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
