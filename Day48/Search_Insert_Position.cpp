261aa8a5502bb7a90472338609e310a70f6c7dc9
Saptarshi Mondal <93252111+Me7rishi@users.noreply.github.com>
2023-02-21 00:02:13 +0530

    Create Search_Insert_Position.cpp

---
+++ Day48/Search_Insert_Position.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+
+    int func(vector<int> nums, int low,int high, int target){
+
+        int mid=(low+high)/2;
+        if(nums[mid]==target){
+            return mid;
+        }
+        else if(low==high){
+            return mid;
+        }
+        else if(nums[mid]>target){
+            return func(nums,low,mid,target);
+        }
+        else{
+            return func(nums,mid+1,high,target);
+        }
+        return -1;
+    }
+
+    int searchInsert(vector<int>& nums, int target) {
+
+        if(target>nums[nums.size()-1]){
+            return nums.size();
+        }
+        return func(nums,0,nums.size(),target);
+    }
+};
