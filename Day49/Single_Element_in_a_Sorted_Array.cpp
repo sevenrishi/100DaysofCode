07ccb4c95665a1eb776aa3781e4b9676db1786ff
Saptarshi Mondal <93252111+Me7rishi@users.noreply.github.com>
2023-02-21 22:15:59 +0530

    Create Single_Element_in_a_Sorted_Array.cpp

---
+++ Day49/Single_Element_in_a_Sorted_Array.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+
+    int func(vector<int> nums, int low,int high){
+
+        int mid=(low+high)/2;
+        if(low==high){
+            return low;
+        }
+        if(mid%2==0 && nums[mid]==nums[mid+1]){
+            return func(nums,mid+1,high);
+        }
+        else if(mid%2==0 && nums[mid]!=nums[mid+1]){
+            return func(nums,low,mid);
+        }
+        else if(mid%2!=0 && nums[mid]==nums[mid-1]){
+            return func(nums,mid+1,high);
+        }
+        return func(nums,low,mid);
+    }
+
+    int singleNonDuplicate(vector<int>& nums) {
+
+        if(nums.size()==1){
+            return nums[0];
+        }
+        int index= func(nums,0,nums.size()-1);
+        return nums[index];
+    }
+};
