5474f72326633b4d82b20891fdb78a51449004e3
Saptarshi Mondal <93252111+Me7rishi@users.noreply.github.com>
2023-02-21 22:18:11 +0530

    Create Fill_the_Matrix.cpp

---
+++ Day49/Fill_the_Matrix.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{   
+public:
+    int minIteration(int N, int M, int x, int y){    
+        // code here
+        int ans=0;
+        for(int i=1; i<=N; i++)
+        {
+            for(int j=1; j<=M; j++)
+            {
+                ans=max(ans,abs(i-x)+abs(j-y));
+            }
+        }
+
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N, M, x, y;
+        cin >> N >> M;
+        cin >> x >> y;
+        
+        Solution ob;
+        cout << ob.minIteration(N, M, x, y) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
