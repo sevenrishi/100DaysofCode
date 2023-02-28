c7d86e224fc6111808dd10e1af8bd81ab093b5ca
Saptarshi Mondal <saptarshimondal768@gmail.com>
2023-02-28 22:21:19 +0530

    Create Optimal_Array.cpp

---
+++ Day56/Optimal_Array.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    vector<int> optimalArray(int n,vector<int> &a){
+        // Code here
+        vector<int>ans;
+        //i=0;
+        ans.push_back(0);
+
+        for(int i=1;i<n; i++)
+        {
+            int new_ans=ans[i-1]+(a[i]-a[i/2]);
+            ans.push_back(new_ans);
+        }
+
+        return ans;
+
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> a(n);
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        Solution obj;
+        vector<int> ans=obj.optimalArray(n,a);
+        for(auto ele:ans){
+            cout<<ele<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
