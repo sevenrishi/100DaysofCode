a556eb214d8a02efc435bf62e4068d3cf2238380
Saptarshi Mondal <saptarshimondal768@gmail.com>
2023-03-10 22:54:15 +0530

    Create Linked_List_Random_Node.cpp

---
+++ Day66/Linked_List_Random_Node.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    ListNode *head = NULL;
+
+    Solution(ListNode* head) {
+        this->head = head;
+    }
+
+    int getRandom() {
+        int ans = 0, i = 1;
+        ListNode *p = this->head;
+        while (p) {
+            if (rand() % i == 0) 
+                ans = p->val;
+            i++;
+            p = p->next;
+        }
+        return ans;
+    }
+};
