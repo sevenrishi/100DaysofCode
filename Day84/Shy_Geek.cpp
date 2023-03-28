1f3e960317be02ffd09d52c6022229303e3cf5c0
Saptarshi Mondal <saptarshimondal768@gmail.com>
2023-03-28 20:23:38 +0530

    Create Shy_Geek.cpp

---
+++ Day84/Shy_Geek.cpp
@@ -0,0 +1,118 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Shop
+{
+    vector<int> chocolates;
+    int countOfCalls;
+    public:
+    Shop()
+    {
+        countOfCalls = 0;
+    }
+    void addChocolate(int x)
+    {
+        chocolates.push_back(x);
+    }
+    long long get(int i)
+    {
+        countOfCalls++;
+        if(i<0 || i>=(int)chocolates.size() || countOfCalls>50)return -1;
+        return chocolates[i];
+    }
+};
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+/*
+Instructions - 
+
+    1. 'shop' is object of class Shop.
+    2. User 'shop.get(int i)' to enquire about the price
+            of the i^th chocolate.
+    3. Every chocolate in shop is arranged in increasing order
+            i.e. shop.get(i) <= shop.get(i + 1) for all 0 <= i < n - 1
+*/
+class Solution{
+    public:
+    Shop shop;
+    Solution(Shop s)
+    {
+        this->shop = s;
+    }
+    long long find(int n, long k){
+        // Return the number of chocolates Geek had
+        // enjoyed out of 'n' chocolates availabe in the
+        // 'shop'.
+        long long ans=0;
+        long long l=0;
+        long long r=n-1;
+
+        while(r>=0 and k>0)
+        {
+            l=0;
+            long long index=-1;
+            long long index_val;
+            while(l<=r)
+            {
+                long long mid=(l+r)/2;
+                long long mid_price=shop.get(mid);
+                if(mid_price<=k)
+                {
+                    index=mid;
+                    index_val=mid_price;
+                    l=mid+1;
+                }
+                else
+                {
+                    r=mid-1;
+                }
+            }
+
+            ans+=(k/index_val);
+            k=k%(index_val);
+            r=index-1;
+
+        }
+
+        return ans;
+    }
+
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int n;
+        scanf("%d",&n);
+        long long k;
+        cin >> k;
+        
+        Shop shop;
+        for(int i = 0; i<n; i++)
+        {
+            int x;
+            cin >> x;
+            shop.addChocolate(x);
+        }
+        
+        Solution obj(shop);
+        long long res = obj.find(n, k);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
