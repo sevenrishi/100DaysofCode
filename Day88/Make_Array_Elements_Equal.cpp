c1e2bc14e2c07a2b395a86f41555d3efb029fb68
Saptarshi Mondal <saptarshimondal768@gmail.com>
2023-04-01 13:28:48 +0530

    Create Make_Array_Elements_Equal.cpp

---
+++ Day88/Make_Array_Elements_Equal.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    long long int minOperations(int N) {
+        // Code here
+        long long int last=2*(N-1)+1;
+        long long mid=(last+1)/2;
+
+        if(mid%2==0)return mid*mid/4;
+
+        return (mid-1)*(mid+1)/4;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.minOperations(n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
