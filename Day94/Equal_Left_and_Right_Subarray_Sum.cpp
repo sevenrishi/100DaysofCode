3edd82153aeada25550be33bfe7d2d6056622dc3
Saptarshi Mondal <saptarshimondal768@gmail.com>
2023-04-06 20:53:41 +0530

    Create Equal_Left_and_Right_Subarray_Sum.cpp

---
+++ Day94/Equal_Left_and_Right_Subarray_Sum.cpp
@@ -0,0 +1,76 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Array
+{
+public:
+    template <class T>
+    static void input(vector<T> &A,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d ",&A[i]);
+        }
+    }
+ 
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int equalSum(int N, vector<int> &A) {
+        long long sum = accumulate(begin(A), end(A), 0);
+        int ans = -1;
+        long long leftsum = A[0];
+        sum -= A[0];
+        if(sum == 0)
+            return 1;
+        for(int i = 1; i < N - 1; i++){
+            sum -= A[i];
+            if(leftsum == sum){
+                ans = i + 1;
+                break;
+            }
+            leftsum += A[i];
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int N; 
+        scanf("%d",&N);
+        
+        
+        vector<int> A(N);
+        Array::input(A,N);
+        
+        Solution obj;
+        int res = obj.equalSum(N, A);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
