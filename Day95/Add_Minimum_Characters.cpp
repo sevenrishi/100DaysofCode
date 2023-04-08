104597239f9003f0de36e38cc36ff13b120989ed
Saptarshi Mondal <saptarshimondal768@gmail.com>
2023-04-08 09:08:57 +0530

    Create Add_Minimum_Characters.cpp

---
+++ Day95/Add_Minimum_Characters.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{   
+public:
+ bool ispalindrome(string s)
+ {
+     int i=0,j=s.size()-1;
+     while(i<j)
+     {
+         if(s[i]!=s[j])
+         return false;
+         else 
+         i++;
+         j--;
+     }
+     return true;
+ }
+    int addMinChar(string str){    
+        //code here
+        if(ispalindrome(str)==true)
+        return 0;
+        for(int i=str.length()-1;i>=0;i--)
+        {
+            if(ispalindrome(str.substr(0,i)))
+            {
+                return str.length()-i;
+            }
+        }
+        return str.length();
+    }
+};
+
+
+//{ Driver Code Starts.
+
+
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string str;
+        cin >> str;
+        
+        Solution ob;
+        cout << ob.addMinChar(str) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
